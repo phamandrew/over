a8e1e92948218d7e110bb2cae22258a6f25643c5
Andrew Pham <andrew.w.pham@gmail.com>
2018-03-05 16:51:18 -0500

    added js file and updated stylings

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+
+$(window).scroll(function(event){
+	didScroll = true;
+
+	$('.projects').each(function(i){
+		var bottomElement = $(this).position().top + $(this).outerHeight();
+		var bottomWindow = $(window).scrollTop() + $(window).height();
+
+		if (bottomWindow > bottomElement - 200){
+			$(this).animate({'opacity':'1'},1000);
+		}
+	});
+});
+
+	// Smooth Scroll
+
+$(document).ready(function(){
+
+	$('.smooth').on('click', function(e){
+		var linkHref = $(this).attr('href');
+
+		$('html, body').animate({
+			scrollTop: $(linkHref).offset().top
+		}, 1500);
+
+		e.preventDefault();
+	});
+
+});
+
+// MENU
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", showMenu, false);
+var hamburgerCollapse = document.querySelector(".hamburger--collapse")
+
+var flyoutMenu = document.querySelector("#flyoutMenu");
+flyoutMenu.addEventListener("click", showMenu, false);
+
+function showMenu(e) {
+	flyoutMenu.classList.toggle("show");
+	hamburgerCollapse.classList.toggle("is-active");
+}
+
+
